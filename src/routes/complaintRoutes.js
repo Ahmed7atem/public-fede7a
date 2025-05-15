74d04629eb9bdf2c7d31aac979785b7d74332fad
ahmed7atem <ahmedhatem.ckae@gmail.com>
2025-05-15 23:27:21 +0300

    ya rab 12

--- src/routes/complaintRoutes.js
+++ src/routes/complaintRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { protect, admin } = require('../middlewares/authMiddleware');
+const { protect, admin } = require('../middleware/auth');
 const {
   getAllComplaints,
   getComplaintById,
